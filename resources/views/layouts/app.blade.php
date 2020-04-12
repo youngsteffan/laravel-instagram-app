aa234550ea3d17ed79b2499d370c42ed457efa16
Igor <wut666@mail.ru>
2020-04-12 18:52:25 +0300

    some styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,5 +84,10 @@
             @yield('content')
         </main>
     </div>
+    <footer class="py-4 bg-dark text-white-50">
+        <div class="container text-center">
+            <small>Copyright &copy; Photowall</small>
+        </div>
+    </footer>
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,16 +4,25 @@
     <div class="container m-auto">
         @foreach($posts as $post)
             <div class="row mt-5">
-                <div class="col-6 offset-1">
-                    <a href="/profile/{{ $post->user->id }}"><img src="/storage/{{ $post->image }}" alt="" class="w-100" style="max-width: 550px;"></a>
+                <div class="col-5 offset-2">
+                    <a href="/p/{{ $post->id }}">
+                        <img src="/storage/{{ $post->image }}" alt="" class="w-100" style="max-width: 550px;">
+                    </a>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="font-weight-lighter post-date pt-1"> {{ date('j F', strtotime($post->created_at)) }}</div>
+                        <div class="d-flex align-items-center">
+                            <span class="font-weight-lighter mr-2" id="js-like-counter">{{ $post->likes->count() }} likes</span>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="col-4 offset">
+                <div class="col-4">
                     <div class="d-flex align-items-center mb-2">
-                        <img src="/storage/{{ $post->user->profile->image }}" alt="" class="w-100 rounded-circle mr-2"
+                        <a href="/profile/{{ $post->user->profile->id }}" class="font-weight-bold" style="color: #000; text-outline: none; text-decoration: none">
+                        <img src="/storage/{{ $post->user->profile->image }}" alt="" class="w-100 rounded-circle mr-2 "
                              style="max-width: 60px;">
-                        <a href="/profile/{{ $post->user->profile->id }}" class="font-weight-bold"
-                           style="color: #000; text-outline: none; text-decoration: none">{{ $post->user->username }}</a>
+                        <span>{{ $post->user->username }}</span>
+                        </a>
                     </div>
 
                     <hr>
