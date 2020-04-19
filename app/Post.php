e7533c612f6ba289a9ab4a7339126387f5c9252a
Igor <wut666@mail.ru>
2020-04-19 17:51:32 +0300

    added comments

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravelista\Comments\Commentable;
 
 class Post extends Model
 {
+
+    use Commentable;
 
     protected $guarded = [];
 
--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Mail;
+use Laravelista\Comments\Commenter;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Commenter;
 
     /**
      * The attributes that are mass assignable.
